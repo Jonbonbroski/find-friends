1f7125c7a840f7cdcbc729a45e42ee1fd7ea723c
Jonbonbroski <jonbonbroski@gmail.com>
2019-08-16 14:18:47 -0700

    apiRoutes error finding module

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,19 +3,17 @@ var path = require("path");
 module.exports = function(app){
 
 app.get("/", function(req,res){
-    res.sendFile(path.join(__dirname, "home.html"))
+    //res.sendFile(path.join(__dirname, "home.html"))
     // res.sendFile(path.join(__dirname, "app\\data\\friends.js"))
+    res.sendFile(path.join(__dirname, "../public/home.html"))
+
 });
 
 app.get("/survey", function (req,res){
-    // res.sendFile(path.join(__dirname, "..\\public\\survey.html"))
-    res.sendFile(path.join(__dirname, "survey.html"))
+     res.sendFile(path.join(__dirname, "../public/survey.html"))
+    //res.sendFile(path.join(__dirname, "survey.html"))
     // res.sendFile(path.join(__dirname, "app\\data\\friends.js"))
 });
 
 
-app.get("/app.js", function(req,res){
-    // res.sendFile(path.join(__dirname, "..\\public\\home.html"))
-    res.sendFile(path.join(__dirname, "app.js"))
-});
 };
